d7d3385933113f39bcedd6d596a31b88744959cf
AlexxSeven <8524552+AlexxSeven@users.noreply.github.com>
2023-10-09 16:55:26 -0500

    Add initial scripts
    Added cam rotato & simple mic source.

---
+++ Cam Rotato/A7CamRotato.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class A7CamRotato : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+    [SerializeField] private float distanceToTarget = 5;
+    [SerializeField] [Range(0, 10)] private float speed = 0;
+    [SerializeField] private Vector3 rotationAxis = new Vector3(0, 1, 0);
+
+    private Vector3 previousPosition;
+
+    void Start()
+    {
+        previousPosition = cam.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = cam.transform.position;
+        Vector3 direction = previousPosition - newPosition;
+
+        float rotationAroundAxis = -direction.x + speed;
+
+        cam.transform.position = target.position;
+
+        cam.transform.Rotate(rotationAxis, rotationAroundAxis, Space.World);
+
+        cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+        previousPosition = newPosition;
+    }
+}
---
+++ Simple Mic Source/A7SimpleMicSource.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class A7SimpleMicSource : MonoBehaviour
+{
+    AudioSource _audioSource;
+
+    //Microphone Input
+    public AudioClip _audioClip;
+    public bool _getDeviceNames;
+    public string _selectedDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if(_getDeviceNames)
+        {
+            Debug.Log("Devices you have available.");
+            foreach (var device in Microphone.devices)
+            {
+                Debug.Log("Name: " + device);
+            }
+        }
+
+        if (Microphone.devices.Length > 0)
+        {
+            if (_selectedDevice == "")
+            {
+                _selectedDevice = Microphone.devices[0].ToString();
+            }
+            _audioSource.clip = Microphone.Start(_selectedDevice, true, 12, AudioSettings.outputSampleRate);
+        }
+        else
+        {
+            _audioSource.clip = _audioClip;
+        }
+        _audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
